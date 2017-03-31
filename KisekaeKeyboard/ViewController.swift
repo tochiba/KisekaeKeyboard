71aad54a631221b8c21fecbf539d265cfd6f0188
tochiba <c104kn@gmail.com>
2017-03-31 09:55:01 +0900

    Initial Commit

---
+++ KisekaeKeyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KisekaeKeyboard
+//
+//  Created by 千葉俊輝 on 2017/03/31.
+//  Copyright © 2017年 Toshiki Chiba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
